8c462324ca7dad77a724aa93bfa90a3217157f83
xsu <suyongkang@gmail.com>
2022-04-27 12:42:40 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/su/wiki/controller/TestController.java
+++ src/main/java/com/su/wiki/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.su.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
@@ -10,8 +9,18 @@ public class TestController {
      * GET POST PUT DELETE
      * @return
      */
-    @RequestMapping("/hello")
+//    @PostMapping
+//    @PutMapping
+//    @DeleteMapping
+//    @RequestMapping(value = "/user/1",method = RequestMethod.GET)
+//    @RequestMapping(value = "/user/1",method = RequestMethod.DELETE)
+    @GetMapping("/hello")
     public String hello(){
         return "Hello,World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "Hello World! Post, " + name;
     }
 }
