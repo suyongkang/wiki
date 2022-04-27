ba02f8477c76b577d9779e438b0b8a34bc69e3f6
xsu <suyongkang@gmail.com>
2022-04-27 10:43:37 +0800

    2-6 开发Hello World接口 访问地址：http://localhost:8080/hello

--- src/main/java/com/su/wiki/config/WikiApplication.java
+++ src/main/java/com/su/wiki/config/WikiApplication.java
@@ -1,13 +1,14 @@
-package com.su.wiki;
+package com.su.wiki.config;
 
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.ConfigurableEnvironment;
 
-
+@ComponentScan("com.su")
 @SpringBootApplication
 public class WikiApplication {
     private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
