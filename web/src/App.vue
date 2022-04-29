2f0bb287fdda185b18e85ffbb13954a57a8b8fc1
xsu <suyongkang@gmail.com>
2022-04-29 13:06:57 +0800

    4-7 制作TheHeader组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,24 +1,23 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <TheHeader/>
     <router-view/>
     <a-layout-footer style="text-align: center">
       Ant Design ©2018 Created by Ant UED
     </a-layout-footer>
   </a-layout>
 </template>
+
+<script>
+import TheHeader from '@/components/TheHeader.vue';
+import {defineComponent} from "vue";
+export default defineComponent({
+  name: 'app',
+  components: {
+    TheHeader,
+  },
+});
+</script>
 
 
 <style>
