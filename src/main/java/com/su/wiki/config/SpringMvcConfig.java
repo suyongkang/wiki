df62d843a559c9248d8f64a1117d35a9a8f6f765
xsu <suyongkang@gmail.com>
2022-04-29 17:21:23 +0800

    5-9 增加AOP，打印接口耗时，请求参数和返回参数 去掉过滤器和拦截器

--- src/main/java/com/su/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/su/wiki/config/SpringMvcConfig.java
@@ -1,21 +1,21 @@
-package com.su.wiki.config;
-
-import com.su.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    private LogInterceptor logInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**");
-    }
-}
+//package com.su.wiki.config;
+//
+//import com.su.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    private LogInterceptor logInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**");
+//    }
+//}
